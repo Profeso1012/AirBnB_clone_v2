e4c67a131b905496979fc7c5ac0f4e1d12d75890
Profeso1012 <itzairso204@gmail.com>
2024-07-14 18:37:19 +0100

    not_server_1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env, put, run
+from fabric.api import env, put, run, sudo, local
 
-env.hosts = ["100.25.165.20", "52.91.133.191"]  # Replace with your actual IPs
+env.hosts = ["100.25.165.20", "52.91.133.191"]
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'  # Replace with your actual key path
-
+env.key_filename = '~/.ssh/id_rsa'
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
@@ -17,28 +16,38 @@ def do_deploy(archive_path):
         Otherwise - True.
     """
     if not os.path.isfile(archive_path):
+        print("Archive path does not exist.")
         return False
+
     try:
         file_name = archive_path.split("/")[-1]
         no_ext = file_name.split(".")[0]
-        # Upload the archive to the /tmp/ directory of the web server
+        release_dir = "/data/web_static/releases/{}".format(no_ext)
+        
+        print("Uploading archive to /tmp/...")
         put(archive_path, "/tmp/{}".format(file_name))
-        # Create the release directory
-        run("mkdir -p /data/web_static/releases/{}".format(no_ext))
-        # Uncompress the archive to the folder
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(
-            file_name, no_ext))
-        # Delete the archive from the web server
+
+        print("Creating release directory: {}".format(release_dir))
+        run("mkdir -p {}".format(release_dir))
+
+        print("Uncompressing archive to release directory...")
+        run("tar -xzf /tmp/{} -C {}".format(file_name, release_dir))
+
+        print("Removing archive from /tmp/...")
         run("rm /tmp/{}".format(file_name))
-        # Move the contents to the correct folder
-        run("mv /data/web_static/releases/{}/web_static/* "
-            "/data/web_static/releases/{}/".format(no_ext, no_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(no_ext))
-        # Delete the current symbolic link
+
+        print("Moving contents to correct folder...")
+        run("mv {0}/web_static/* {0}/".format(release_dir))
+        run("rm -rf {}/web_static".format(release_dir))
+
+        print("Removing current symbolic link...")
         run("rm -rf /data/web_static/current")
-        # Create a new symbolic link
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".
-            format(no_ext))
+
+        print("Creating new symbolic link...")
+        run("ln -s {} /data/web_static/current".format(release_dir))
+
+        print("Deployment successful.")
         return True
-    except Exception:
+    except Exception as e:
+        print("Deployment failed:", e)
         return False
