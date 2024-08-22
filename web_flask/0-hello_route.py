c470a2d60e6e92da841abf1721b87b4dd4f85437
Profeso1012 <itzairso204@gmail.com>
2024-08-22 19:47:06 +0000

    gunicorn-01

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,11 +8,12 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/airbnb-onepage', strict_slashes=False)
 def hello_hbnb():
     """Route that returns a simple greeting."""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
+    #app.run()
     app.run(host="0.0.0.0", port=5000)
